d30e34cac7fe7911f088a8c232ec38cc1dd7fa05
SreyVenk <157976631+SreyVenk@users.noreply.github.com>
2026-04-01 08:48:43 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        somehash = {}
+
+        for word in strs:
+            key = "".join(sorted(word))
+            if key not in somehash:
+                somehash[key] = []
+            somehash[key].append(word)
+        return list(somehash.values())
